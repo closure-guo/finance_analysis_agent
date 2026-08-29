75c91975ce37b49253c1be54b9814364f9470a15
Closure <17624021080@163.com>
2026-08-29 23:24:42 +0800

    feat(frontend): 补 tailwindcss-animate 插件

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+import animate from 'tailwindcss-animate'
+
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
@@ -55,5 +57,5 @@ export default {
       },
     },
   },
-  plugins: [],
+  plugins: [animate],
 }
